df7af11dae327e382c9e87984df52daae35ba40f
Sharit-Vargas07 <sharitvargas07@gmail.com>
2023-06-13 19:47:53 -0500

    cambios

---
+++ js/toggle.js
@@ -0,0 +1,16 @@
+let circuloBox = document.getElementById("circuloBox"); /* Circulo */
+let toggleBox = document.getElementById("toggleBox"); /* Rectangulo */
+/* Verificamos que la clase se encuentra en choice-box */
+circuloBox.addEventListener("click", function(){
+  if (circuloBox.classList.contains("circuloBox")) {
+    circuloBox.classList.add("circulo-box-on")
+    circuloBox.classList.remove("circuloBox")
+    toggleBox.classList.add("toggle-box-on")
+    
+  }
+  else{
+    circuloBox.classList.add("circuloBox")
+    circuloBox.classList.remove("circulo-box-on")
+    toggleBox.classList.remove("toggle-box-on")
+  }
+})
